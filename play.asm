03a653f5c77239f328323df76dd089a88eff524c
Lucassec1 <lucasemmanuelcs@gmail.com>
2024-02-07 00:36:47 -0300

    add play function

--- play.asm
+++ play.asm
@@ -3,12 +3,13 @@
 .globl play
 
 play:
-	save_context # salva os dados da memória
+	save_context
 	
-	move $s3, $a2 #board
-	move $s1, $a0 # row
-	move $s2, $a1 # column
+	move $s3, $a2 # Pointer to array (board)
+	move $s1, $a0 # Row
+	move $s2, $a1 # Column
 	
+	# Calculates cell address in array
 	li $t0, SIZE
 	mul $t0, $t0, $s1
 	add $t0, $t0, $s2
@@ -16,16 +17,26 @@ play:
 	mul $t0, $t0, $t1
 	add $t0, $t0, $s3
 			
-	lw $t0, 0($t0) # board[i][j]
+	lw $t0, 0($t0) # Load the value in cell (board[i][j])
 			
 	li $t1, -1
 	
+	# Verify if the cell contain a bomb
 	beq $t0, $t1, falha_condicao
 	
+	# If the cell does not contain a bomb, call revealAdjacentCells
+	move $a0, $s1 # Pass the row as argument
+	move $a1, $s2 # Pass the column as argument
+	move $a2, $s3 # Pass the pointer to the array as argument
+	jal revealNeighboringCells
+	
+	# Restore the context and returns 1 (game continue)
+	li $v0, 1
 	restore_context
+	jr $ra 
 	
 falha_condicao:
+	# If the call contain a bomb, return 0 (end game)
 	li $v0, 0
 	restore_context
-	
 	jr $ra 
